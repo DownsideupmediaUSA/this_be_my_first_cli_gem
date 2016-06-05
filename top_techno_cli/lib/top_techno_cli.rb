5b22bfba844e0927aae8547a5034cec290bda5df
DownsideupmediaUSA <brandondecarlo@live.com>
2016-06-05 14:38:58 -0400

    added more functionality to cli including list tracks and menu methods

--- top_techno_cli/lib/top_techno_cli.rb
+++ top_techno_cli/lib/top_techno_cli.rb
@@ -9,14 +9,40 @@ class TopTechnoCli::CLI
  def call
   puts "Want Top Techno???"
   list_tracks
+  menu
+  goodbye
 end
 
 def list_tracks
 puts <<-DOC
-   1. Brandon De Carlo - Half of What
-   2. Derrick May - Strings of Life
-   3. Plastikman - Spastik 
+   1. Brandon De Carlo - Half of What - Beatport
+   2. Derrick May - Strings of Life - Juno
+   3. Plastikman - Spastik - Traxsource
    DOC
  end
+
+def menu
+  puts "Please enter the number of the track you would like more info on or type list to see a list of the top tracks again or type exit to enter:"
+  input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+    case input
+    when "1"
+      puts "more info on track 1..."
+    when "2"
+      puts "more info on track 2..."
+    when "3"
+      puts "more info on track 3..."
+    when "list"
+      list_tracks
+
+    end
+  end
+end
+
+def goodbye
+  puts "See you next week for nothin but Top Techno!"
+end
+
 
 end
